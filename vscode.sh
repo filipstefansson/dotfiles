0467968ad7dc1d27a1edcda632341d1ba42b9309
Filip Stefansson <filip.stefansson@gmail.com>
2021-02-01 07:22:20 +0100

    feat(vscode): update extensions

--- vscode.sh
+++ vscode.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 # Plugins
-code --install-extension esbenp.prettier-vscode
-code --install-extension alefragnani.project-manager
-code --install-extension dbaeumer.vscode-eslint
+code --install-extension patbenatar.advanced-new-file
+code --install--extension bungcip.better-toml
 code --install-extension wmaurer.change-case
 code --install-extension mikestead.dotenv
-code --install-extension eamodio.gitlens
-code --install-extension patbenatar.advanced-new-file
 code --install-extension dracula-theme.theme-dracula
-code --install-extension shan.code-settings-sync
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install--extension graphql.vscode-graphql
+code --install-extension esbenp.prettier-vscode
+code --install--extension bungcip.prisma.prisma
+code --install-extension alefragnani.project-manager
+code --install--extension bungcip.matklad.rust-analyzer
+code --install--extension whtouche.vscode-js-console-utils
+
