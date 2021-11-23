6710f04810bb3be3bbdf8557be434f8e5e28426b
Filip Stefansson <filip.stefansson@gmail.com>
2021-11-23 19:34:49 +0100

    fix: add back oh-my-zsh

--- zsh.sh
+++ zsh.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
 # Plugins
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 git clone https://github.com/paulirish/git-open.git $HOME/.oh-my-zsh/plugins/git-open
